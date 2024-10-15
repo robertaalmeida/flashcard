36980462a62d0076069e6bef4c1644b652174f1f
almeidaroberta <148370745+robertaalmeida@users.noreply.github.com>
2024-10-15 15:46:06 -0300

    vjkhj

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Progamação',
+    'O que é Python?',
+    'O Python é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem é considerado pai da genética?',
+    'Gregor Mendel'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de Pi?',
+    '3,14'
+)
