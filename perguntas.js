9a187e49647d4aa5cce20e3c80dae24c149b1c4b
almeidaroberta <148370745+robertaalmeida@users.noreply.github.com>
2024-11-05 15:55:18 -0300

    thr

--- perguntas.js
+++ perguntas.js
@@ -1,55 +1,32 @@
 criaCartao(
-    'Genética',
-    'O que é a Primeira Lei de Mendel?',
-    'A Primeira Lei de Mendel afirma que cada indivíduo tem dois alelos por característica, herdados de pai e mãe, que se separam durante a formação dos gametas, garantindo a variabilidade genética.'
+    'Maquiagem',
+    'Para que serve a base da maquiagem?',
+    'A base serve para uniformizar o tom da pele, cobrir imperfeições e proporcionar uma superfície lisa para aplicação de outros produtos'
 )
 
 criaCartao(
-   'Genética',
-    'Como funciona a Primeira Lei de Mendel em um exemplo simples?',
-    'Cruzando plantas de ervilha amarelas (AA) com verdes (aa), a primeira geração (F1) terá sementes amarelas (Aa). Na segunda geração (F2), a proporção será 3 amarelas para 1 verde.'
-    )
+   'Maquiagem',
+    'Quais são os principais tipos de pincéis de maquiagem? ',
+    'São pincéis para base, pó, blush, sombra e lábios'
+criaCartao(
+    'Maquiagem',
+    'Como aplicar sombra nos olhos?',
+    'Aplique sombra clara na parte interna do olho, sombra média no meio e sombra escura na parte externa, blendendo bem')
 
 criaCartao(
-    'Genética',
-    'Como a ideia de segregação independente se relaciona com a Primeira Lei de Mendel?',
-    'A Segunda Lei de Mendel aborda a segregação independente de diferentes características. A Primeira Lei também implica que os genes para uma característica se separam independentemente durante a formação dos gametas.')
-
-criaCartao(
-    'Genética',
-    'Qual a Segunda Lei de Mendel?',
-    'A Segunda Lei de Mendel afirma que alelos de diferentes genes se segregam independentemente durante a formação dos gametas, significando que a herança de um gene não afeta a de outro gene.'
+    'Maquiagem',
+    'Qual é o objetivo do corretor?',
+    'O corretor serve para cobrir olheiras, manchas e imperfeições específicas.'
 )
 
 criaCartao(
-    'Genética',
-    'Como Mendel descobriu a Segunda Lei?',
-    'Mendel, ao estudar ervilhas em cruzamentos diíbridos, notou que a herança de uma característica não influenciava a outra. Assim, ele concluiu que os alelos de diferentes genes se segregam independentemente.'
+    'Maquiagem',
+    'Quais sãõ os passos básicos para uma maquiagem diária?',
+    'Base, pó, blush, sombra, máscara de cílios e batom.'
 )
 
 criaCartao(
-    'Genética',
-    'Considere uma planta de ervilha que é heterozigota para duas características: cor da semente (amarela é dominante sobre verde) e forma da semente (lisa é dominante sobre rugosa). Qual será a proporção fenotípica esperada na geração F2?',
-    '9:3:3:1 (9 amarelas lisas : 3 amarelas rugosas : 3 verdes lisas : 1 verde rugosa).'
+    'Maquiagem',
+    'Como escolher a cor certa de batom?',
+    'Escolha uma cor que combine com o tom da pele e o estilo pessoal.'
 )
-
-
-criaCartao(
-    'Genética',
-    'Oque é Co-odominância?',
-    'Co-dominância é um padrão de herança onde ambos os alelos de um par são expressos igualmente no fenótipo. Um exemplo é o tipo sanguíneo AB, que combina características dos alelos A e B.'
-)
-
-criaCartao(
-    'Genética',
-    'Oque é Heredogramas?',
-    'Heredogramas são representações gráficas que mostram a herança de características genéticas em famílias ao longo de gerações, permitindo identificar padrões de herança e a presença de doenças genéticas.'
-)
-
-criaCartao(
-    'Genética',
-    'Qual é a diferença entre Genótipo e fenótipo?',
-    'Genótipo é a composição genética de um organismo, representando a combinação de alelos para um gene. Fenótipo, por outro lado, são as características observáveis resultantes da interação entre o genótipo e o ambiente, como cor, forma e comportamento.'
-)
-
-
